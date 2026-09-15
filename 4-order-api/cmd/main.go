fdd2cd7266681d112b45e4509162e55ed5daede9
Ivan Ezhov <ivan.ezhov.94@gmail.com>
2026-09-15 14:50:46 +0300

    HW-6: logger

--- 4-order-api/cmd/main.go
+++ 4-order-api/cmd/main.go
@@ -4,14 +4,24 @@ import (
 	"4-order-api/configs"
 	"4-order-api/internal/product"
 	"4-order-api/pkg/db"
+	"4-order-api/pkg/middleware"
 	"fmt"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
 
 func main() {
 	config := configs.LoadConfig()
 	router := http.NewServeMux()
 	database := db.NewDb(config)
+
+	// Middlewares
+	mds := middleware.Chain(middleware.CORS, middleware.Logging)
 
 	// Repositories
 	productRepository := product.NewProductRepository(database)
@@ -22,7 +32,7 @@ func main() {
 	// Server
 	server := http.Server{
 		Addr:    ":" + config.AppPort,
-		Handler: router,
+		Handler: mds(router),
 	}
 	fmt.Printf("Server is listening on port %v\n", config.AppPort)
 	server.ListenAndServe()
